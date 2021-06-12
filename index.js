4faa1963309788dfa9c2452167f631e0b7f6aec7
KarinKwint <83405911+KarinKwint@users.noreply.github.com>
2021-06-12 16:12:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const age = 36;
+const isFemale = true;
+const DriverStatus = "bob";
+const Name = "Abraham"
+const Name = "Sarah"
+const TotalAmount = 25
+const TotalAmount = 50
+const TotalAmount = 100
+
+
+if (age >= 18) {
+    console.log("Welcome");
+} else {
+    console.log("Sorry, you are not allowed");
+}
+
+if (isFemale) {
+    console.log("to ladysnight");
+} else {
+    console.log("Sorry it is ladysnight tonight");
+}
+
+if (DriverStatus == "bob") {
+    console.log("and drive save home");
+} else {
+    console.log("Please give me your keys and have a good night");
+}
+
+if (Age >= 18 && age <= 25) {
+    console.log("Je krijgt 50% korting!");
+}
+
+if (Name == "Abraham" || Name == "Sarah") {
+    console.log("Yeah, you will get a free beer tonight");
+}
+
+if (TotalAmount > 25 && TotalAmount < 50) {
+    console.log("Congrats, you will get vegan balls");
+} else if (TotalAmount > 50 && TotalAmount < 100) {
+    console.log("Congrats, you will get nachos");
+} else if (TotalAmount > 100) {
+    console.log("Congrats, you will get champaigne");
+} else {
+    console.log("No extras for you");
+}
